a408a4d4409e524e92e5b16c2d6ebe56d5a7d6c2
Travis <tlneondo@gmail.com>
2023-11-15 15:22:17 -0500

    more additions

--- dvdtoh265.sh
+++ dvdtoh265.sh
@@ -3,3 +3,17 @@ for file in *; do
         echo "$file" 
     fi 
 done
+
+ffmpeg -i input -c:v libx265 -crf 28 -preset medium -c:a libvorbis output.mp4
+
+ffmpeg -i input -c:v libx265 -crf 26 -preset fast -c:a aac -b:a 128k output.mp4
+##what I want
+##to take dvd copy of files dumped from DVD
+##Encode audio to opus or passthrough
+##encode video as exact framerate
+
+-tune animation
+
+current command: 
+
+ffmpeg -i input -c:v libx265 -crf 26 -preset slow -c:a aac -b:a 128k output.mp4fast
